2e3d83942c8f19f2ed499917c0143e2a05d6e8e8
Ethan Portelli <159474174+EthanPortelli@users.noreply.github.com>
2024-11-24 13:53:42 -0500

    Update Routes.js

--- Ethan's Stuff/Routes.js
+++ Ethan's Stuff/Routes.js
@@ -7,6 +7,7 @@ router.post("/login", Controller.login);
 router.get("/inventory", Controller.inventory);
 router.post("/add-item", Controller.addItem);
 router.delete("/delete-item/:id", Controller.deleteItem);
+router.patch("/update-item/:id", Controller.updateItem);
 
 // Export the router
 module.exports = router;
